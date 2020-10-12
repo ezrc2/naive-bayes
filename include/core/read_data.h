5dac72d778819f48efecc3fa1b454fe9aff2f17d
ezrc2 <ezchen2556@gmail.com>
2020-10-12 16:29:11 -0700

    Removed file path member variables

--- include/core/read_data.h
+++ include/core/read_data.h
@@ -1,26 +1,20 @@
 #pragma once
 
-#include <vector>
-#include <map>
+#include <fstream>
 #include <iostream>
 #include <istream>
-#include <ostream>
+#include <map>
 #include <string>
+#include <vector>
+
+#include "image.h"
 
 class Parser {
+
  public:
 
-  Parser();
-
-  friend std::istream &operator>>(std::istream &input, std::string &file);
-
-  const size_t kImageSize = 28;
-  std::multimap<int, std::vector<std::vector<char>>> pairs_;
-
- private:
-  size_t line_number_;
-  std::string path_images_;
-  std::string path_labels_;
+  std::vector<size_t> GetLabelsFromFile();
+  std::vector<Image> GetImagesFromFile();
 
 
 };
