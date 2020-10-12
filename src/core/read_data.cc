c1c9d9b4105727843bb32a4a668a5b0b07c1c0a8
ezrc2 <ezchen2556@gmail.com>
2020-10-12 16:28:45 -0700

    Added function to get a vector of training labels

--- src/core/read_data.cc
+++ src/core/read_data.cc
@@ -1,24 +1,27 @@
 #include <core/read_data.h>
 
-Parser::Parser() {
-  std::cout << "Enter file path to training images" << std::endl;
-  std::getline(std::cin, path_images_);
 
-  std::cout << "Enter file path to training labels" << std::endl;
-  std::getline(std::cin, path_labels_);
+std::vector<size_t> Parser::GetLabelsFromFile() {
+  std::string path;
+  std::cout << "Enter file path of training labels" << std::endl;
+  std::getline(std::cin, path);
 
-  line_number_ = 0;
-}
-
-std::istream &operator>>(std::istream &input, Parser &parser) {
-  std::vector<std::vector<char>> pixels;
-  std::vector<char> row;
-  for (size_t row = 0; row < parser.kImageSize; row++) {
-    std::string line;
-    std::getline(input, line);
-    for (size_t col = 0; col < parser.kImageSize; col++) {
-      parser.pairs_.
-    }
+  std::ifstream file_reader(path);
+  // Check if file doesn't exist
+  while (!file_reader) {
+    std::cout << "File doesn't exist." << std::endl;
+    std::cout << "Enter file path of training labels" << std::endl;
+    std::getline(std::cin, path);
+    file_reader.open(path);
   }
+  std::string line;
+
+  std::vector<size_t> training_labels;
+  while (std::getline(file_reader, line)) {
+    training_labels.push_back(std::stoi(line)); // convert to int
+  }
+
+  return training_labels;
 }
+
 
