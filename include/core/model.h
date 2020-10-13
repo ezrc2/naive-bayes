6fcde91112ee501a8c9a24981ca4aa7385cc0ab8
ezrc2 <ezchen2556@gmail.com>
2020-10-13 10:41:57 -0700

    Added code to calculate feature probabilities

--- include/core/model.h
+++ include/core/model.h
@@ -7,16 +7,18 @@
 
 class Classifier {
  public:
+
   Classifier(std::multimap<size_t, Image> &pairs, size_t image_size);
 
   void CalculateClassProbabilities();
   void CalculateFeatureProbabilities();
 
   std::vector<double> GetClassProbabilities();
+  std::vector<std::vector<std::vector<double>>> GetFeatureProbabilities();
 
  private:
 
-
+  void ApplyLaplaceSmoothing();
 
   std::multimap<size_t, Image> image_label_pairs_;
   std::vector<size_t> images_per_class_;
@@ -25,6 +27,7 @@ class Classifier {
       feature_probabilities_;  // shaded pixels
 
   size_t image_size_;
+  size_t total_training_images_;
   const size_t kLapLaceSmoothing = 1;
   const size_t kNumberOfClasses = 10;
   const char kWhitePixel = ' ';
--- src/core/model.cc
+++ src/core/model.cc
@@ -10,7 +10,7 @@ Classifier::Classifier(std::multimap<size_t, Image> &pairs, size_t image_size) {
     images_per_class_[it->first]++;
   }
 
-  //set all to 0
+  // set all to 0
   for (size_t class_value = 0; class_value < image_size; class_value++) {
     std::vector<std::vector<double>> features;
     for (size_t row = 0; row < image_size; row++) {
@@ -28,23 +28,46 @@ Classifier::Classifier(std::multimap<size_t, Image> &pairs, size_t image_size) {
 }
 
 void Classifier::CalculateClassProbabilities() {
-  double sum = 0;
+  total_training_images_ = 0;
   for (size_t num_images : images_per_class_) {
-    sum += num_images;
+    total_training_images_ += num_images;
   }
 
   // Laplace smoothing
   for (size_t i = 0; i < images_per_class_.size(); i++) {
     class_probabilities_.push_back(
         (kLapLaceSmoothing + images_per_class_[i]) /
-        (kNumberOfClasses * kLapLaceSmoothing + sum));
+        (kNumberOfClasses * kLapLaceSmoothing + total_training_images_));
   }
 }
 
 void Classifier::CalculateFeatureProbabilities() {
+  for (auto &pair : image_label_pairs_) {
+    int class_value = pair.first;
+    std::vector<std::vector<char>> pixels = pair.second.GetPixels();
+
+    for (size_t row = 0; row < image_size_; row++) {
+      for (size_t col = 0; col < image_size_; col++) {
+        char pixel = pixels[row][col];
+        // increment count at (row, col) for this class if pixel is shaded
+        if (pixel == kGreyPixel || pixel == kBlackPixel) {
+          feature_probabilities_[class_value][row][col]++;
+        }
+      }
+    }
+  }
+  ApplyLaplaceSmoothing();
+}
+
+void Classifier::ApplyLaplaceSmoothing() {
 
 }
 
 std::vector<double> Classifier::GetClassProbabilities() {
   return class_probabilities_;
+}
+
+std::vector<std::vector<std::vector<double>>>
+Classifier::GetFeatureProbabilities() {
+  return feature_probabilities_;
 }
