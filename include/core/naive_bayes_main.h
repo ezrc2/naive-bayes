b7d29cdf7b9897540c5177b0acc76ba780b7af51
ezrc2 <ezchen2556@gmail.com>
2020-10-13 10:42:43 -0700

    Added main method code to run everything

--- include/core/naive_bayes_main.h
+++ include/core/naive_bayes_main.h
@@ -1,22 +1,22 @@
+#pragma once
+
 #include <string>
+#include "read_data.h"
+#include <core/model.h>
 
 namespace naivebayes {
 
-class Placeholder {
+class Main {
+
  public:
-  std::string GetBestClass() const;
+
+  void TrainModel();
+  void WriteToFile();
+
+ private:
+
+  Parser parser_;
+
 };
 
 }  // namespace naivebayes
-
-/*
-TODO: rename this file. You'll also need to modify CMakeLists.txt.
-
-You can (and should) create more classes and files in include/core (header
- files) and src/core (source files); this project is too big to only have a
- single class.
-
-Make sure to add any files that you create to CMakeLists.txt.
-
-TODO Delete this comment before submitting your code.
-*/
--- src/core/naive_bayes_main.cc
+++ src/core/naive_bayes_main.cc
@@ -2,8 +2,32 @@
 
 namespace naivebayes {
 
-std::string Placeholder::GetBestClass() const {
-  return "CS 126";
+void Main::TrainModel() {
+  std::multimap<size_t, Image> pairs = parser_.getImageLabelPairs();
+  size_t image_size = parser_.GetImageSize();
+
+  Classifier classifier(pairs, image_size);
+
+  classifier.CalculateClassProbabilities();
+  classifier.CalculateFeatureProbabilities();
+
+  std::vector<double> class_probabilities = classifier.GetClassProbabilities();
+  std::vector<std::vector<std::vector<double>>> features =
+      classifier.GetFeatureProbabilities();
+
+  for (size_t class_value = 0; class_value < features.size(); class_value++) {
+    for (size_t row = 0; row < image_size; row++) {
+      for (size_t col = 0; col < image_size; col++) {
+        std::cout << features[class_value][row][col] << " ";
+      }
+      std::cout << std::endl;
+    }
+    std::cout << std::endl;
+  }
+
+}
+
+void Main::WriteToFile() {
 }
 
 }  // namespace naivebayes
