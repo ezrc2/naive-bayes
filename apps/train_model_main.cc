fcdbe2f5b398b4b0ff57ffeebe3052243144d61b
ezrc2 <ezchen2556@gmail.com>
2020-10-13 19:28:07 -0700

    Edited main method

--- apps/train_model_main.cc
+++ apps/train_model_main.cc
@@ -1,13 +1,7 @@
-#include <iostream>
+#include <core/naive_bayes_main.h>
 
-#include <core/rename_this_file.h>
-
-// TODO: You may want to change main's signature to take in argc and argv
 int main() {
-  // TODO: Replace this with code that reads the training data, trains a model,
-  // and saves the trained model to a file.
-
-  std::cout << "Welcome to " << naivebayes::Placeholder().GetBestClass()
-            << std::endl;
+  naivebayes::Main main;
+  main.TrainModel();
   return 0;
 }
