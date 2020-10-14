b54704971f3992cd8c17f9c4cdc30d79762a2c18
ezrc2 <ezchen2556@gmail.com>
2020-10-13 21:56:07 -0700

    Added tests for smaller image size

--- tests/naive_bayes_test.cc
+++ tests/naive_bayes_test.cc
@@ -1,8 +1,8 @@
 #pragma once
 #include <core/naive_bayes_main.h>
 
-#include <ostream>
 #include <catch2/catch.hpp>
+#include <ostream>
 
 Parser parser;
 
@@ -62,4 +62,36 @@ TEST_CASE("Test GetPixels") {
   std::vector<std::vector<char>> pixels = pairs.find(5)->second.GetPixels();
   REQUIRE(pixels.size() == 28);
   REQUIRE(pixels[0].size() == 28);
+}
+
+TEST_CASE("3x3 images from prairielearn") {
+  SECTION("Class probabilities") {
+    std::vector<double> expected = {0.4, 0.6};
+    std::vector<double> calculated(model.GetClassProbabilities());
+    REQUIRE_THAT(calculated, Catch::Approx(expected).epsilon(1.e-5));
+  }
+  SECTION("Shaded Probabilities for class = 0") {
+    std::vector<std::vector<double>> expected = {
+        {0.666666, 0.666666, 0.666666},
+        {0.666666, 0.333333, 0.666666},
+        {0.666666, 0.666666, 0.666666}};
+
+    std::vector<std::vector<std::vector<double>>> calculated(
+        model.GetFeatureProbabilities());
+
+    REQUIRE_THAT(calculated[0][0], Catch::Approx(expected[0]).epsilon(1.e-5));
+    REQUIRE_THAT(calculated[0][1], Catch::Approx(expected[1]).epsilon(1.e-5));
+    REQUIRE_THAT(calculated[0][2], Catch::Approx(expected[2]).epsilon(1.e-5));
+  }
+  SECTION("Shaded Probabilities for class = 1") {
+    std::vector<std::vector<double>> expected = {
+        {0.5, 0.75, 0.25}, {0.25, 0.75, 0.25}, {0.5, 0.75, 0.5}};
+
+    std::vector<std::vector<std::vector<double>>> calculated(
+        model.GetFeatureProbabilities());
+
+    REQUIRE_THAT(calculated[0][0], Catch::Approx(expected[0]).epsilon(1.e-5));
+    REQUIRE_THAT(calculated[0][1], Catch::Approx(expected[1]).epsilon(1.e-5));
+    REQUIRE_THAT(calculated[0][2], Catch::Approx(expected[2]).epsilon(1.e-5));
+  }
 }
