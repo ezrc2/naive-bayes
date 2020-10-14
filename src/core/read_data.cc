1bc7deee087f914804936525fe824aa6307f4ab9
ezrc2 <ezchen2556@gmail.com>
2020-10-14 16:35:31 -0700

    Fixed file-reading bounds issue

--- src/core/read_data.cc
+++ src/core/read_data.cc
@@ -1,18 +1,15 @@
 #include <core/read_data.h>
 
 void Parser::GetLabelsFromFile() {
-  training_labels_.clear();
-
   std::string path;
-  std::cout << "Enter file path of training labels" << std::endl;
+  std::cout << "Enter file path of training labels:" << std::endl;
   std::getline(std::cin, path);
-
   std::ifstream file_reader(path);
 
   // Check if file doesn't exist
   while (!file_reader) {
     std::cout << "File doesn't exist." << std::endl;
-    std::cout << "Enter file path of training labels" << std::endl;
+    std::cout << "Enter file path of training labels:" << std::endl;
     std::getline(std::cin, path);
     file_reader.open(path);
   }
@@ -26,10 +23,8 @@ void Parser::GetLabelsFromFile() {
 }
 
 void Parser::GetImagesFromFile() {
-  training_images_.clear();
-
   std::string path;
-  std::cout << "Enter file path of training images" << std::endl;
+  std::cout << "Enter file path of training images:" << std::endl;
   std::getline(std::cin, path);
 
   std::ifstream file_reader(path);
@@ -37,25 +32,27 @@ void Parser::GetImagesFromFile() {
   // check if file doesn't exist;
   while (!file_reader) {
     std::cout << "File doesn't exist." << std::endl;
-    std::cout << "Enter file path of training images" << std::endl;
+    std::cout << "Enter file path of training images:" << std::endl;
     std::getline(std::cin, path);
     file_reader.open(path);
   }
 
-  while (!file_reader.eof()) {
-    Image image;
-    file_reader >> image;
-    training_images_.push_back(image);
-  }
-
-  file_reader.close();
-  file_reader.clear();
-
-  // Re-open file to read length of first line and get the size of the image
-  file_reader.open(path);
+  // Read length of first line and get the size of the image
   std::string line;
   std::getline(file_reader, line);
   image_size_ = line.length();
+
+  // Close and reopen file to start from the top
+  file_reader.close();
+  file_reader.clear();
+  file_reader.open(path);
+
+  while (!file_reader.eof()) {
+    Image image;
+    image.SetImageSize(image_size_);
+    file_reader >> image;
+    training_images_.push_back(image);
+  }
 
   file_reader.close();
 }
