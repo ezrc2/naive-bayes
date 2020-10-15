ded2f774774a77539a1eba59c5e158119e6d8909
ezrc2 <ezchen2556@gmail.com>
2020-10-14 18:06:44 -0700

    Added print statement where the output file already has data

--- src/core/naive_bayes_main.cc
+++ src/core/naive_bayes_main.cc
@@ -10,14 +10,14 @@ void Main::TrainModel() {
   Model model(pairs, image_size);
 
   std::string path;
-  std::cout << "Enter file path of file to save trained model" << std::endl;
+  std::cout << "Enter file path of file to save trained model: " << std::endl;
   std::getline(std::cin, path);
 
   std::ifstream file_reader(path);
   // Check if file doesn't exist;
   while (!file_reader) {
     std::cout << "File doesn't exist." << std::endl;
-    std::cout << "Enter file path of file to save trained model" << std::endl;
+    std::cout << "Enter file path of file to save trained model:" << std::endl;
     std::getline(std::cin, path);
     file_reader.open(path);
   }
@@ -28,12 +28,12 @@ void Main::TrainModel() {
     model.CalculateClassProbabilities();
     model.CalculateFeatureProbabilities();
 
-    std::vector<double> class_probabilities(model.GetClassProbabilities());
-    std::vector<std::vector<std::vector<double>>> feature_probabilities(
-        model.GetFeatureProbabilities());
-
-    WriteToFile(path, class_probabilities, feature_probabilities);
+    WriteToFile(path, model.GetClassProbabilities(),
+                model.GetFeatureProbabilities());
+  } else {
+    std::cout << "File already contains model data." << std::endl;
   }
+
   file_reader.close();
 }
 
@@ -41,13 +41,13 @@ void Main::WriteToFile(std::string &path,
                        const std::vector<double> &class_probabilities,
                        const std::vector<std::vector<std::vector<double>>>
                            &feature_probabilities) {
-
   std::ofstream file_writer(path);
 
   // Write the class probabilities first
   for (double probability : class_probabilities) {
     file_writer << probability << std::endl;
   }
+  file_writer << std::endl;
 
   // A row of 2D double vectors for each class
   for (size_t class_value = 0; class_value < feature_probabilities.size();
@@ -56,9 +56,7 @@ void Main::WriteToFile(std::string &path,
          row++) {
       for (size_t col = 0; col < feature_probabilities[class_value].size();
            col++) {
-
         file_writer << feature_probabilities[class_value][row][col] << " ";
-
       }
       file_writer << std::endl;
     }
