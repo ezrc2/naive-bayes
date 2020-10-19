71dbe569f754d8031ebb1c09b285620c9cd2c34e
ezrc2 <ezchen2556@gmail.com>
2020-10-19 16:34:53 -0700

    Added method to load a model from a file

--- include/core/driver.h
+++ include/core/driver.h
@@ -1,10 +1,13 @@
 #pragma once
 
 #include <core/model.h>
+#include <core/classifier.h>
 
 #include <iostream>
 #include <ostream>
 #include <string>
+#include <sstream>
+#include <iterator>
 
 #include "read_data.h"
 
@@ -18,10 +21,22 @@ namespace naivebayes {
 class Driver {
  public:
   /**
-   * Takes in the file paths of the training labels and images and calls the
-   * methods in Model class to train the model
+   * Takes in the file paths of the training labels and images, calls the
+   * methods in Model class to train the model, and saves the model to a file
+   *
+   * @param labels_path
+   * @param images_path
+   * @param saved_path
    */
-  void TrainModel();
+  void TrainModel(const std::string &labels_path, const std::string &images_path, const std::string &saved_path, bool test);
+
+  /**
+   * Loads in a trained model
+   * @param saved_path The file path of the model
+   */
+  void LoadModel(const std::string &saved_path);
+
+  void Classify();
 
  private:
   /**
@@ -32,10 +47,25 @@ class Driver {
    * @param class_probabilities The vector of class probabilities
    * @param feature_probabilities The 3D vector of feature probabilities
    */
-  void WriteToFile(std::string &path,
-                   const std::vector<double> &class_probabilities,
-                   const std::vector<std::vector<std::vector<double>>>
+  void WriteToFile(const std::string &saved_path,
+                   const std::map<size_t, double> &prior_probabilities,
+                   const std::map<size_t, std::vector<std::vector<double>>>
                        &feature_probabilities);
+
+  /**
+   * Splits a string by a delimiter
+   *
+   * @param string The input string
+   * @param delimiter The delimiter
+   * @return A vector of string tokens
+   */
+  std::vector<std::string> SplitString(std::string string, char delimiter);
+
+  std::map<size_t, double> prior_probabilities_;
+  std::map<size_t, std::vector<std::vector<double>>> feature_probabilities_;
+
+  const char kSpace = ' ';
+
 };
 
 }  // namespace naivebayes
--- src/core/driver.cc
+++ src/core/driver.cc
@@ -2,67 +2,102 @@
 
 namespace naivebayes {
 
-void Driver::TrainModel() {
-  Parser parser;
-  std::map<size_t, std::vector<Image>> training_data = parser.GetLabelImagePairs();
-  size_t image_size = parser.GetImageSize();
+void Driver::TrainModel(const std::string &labels_path,
+                        const std::string &images_path,
+                        const std::string &saved_path, bool test) {
+  FileParser file_parser;
+  std::map<size_t, std::vector<Image>> training_data =
+      file_parser.GetLabelImagePairs(labels_path, images_path);
+  size_t image_size = file_parser.GetImageSize();
 
   Model model(training_data, image_size);
 
-  std::string path;
-  std::cout << "Enter file path of file to save trained model: " << std::endl;
-  std::getline(std::cin, path);
+  std::ifstream file_reader(saved_path);
 
-  std::ifstream file_reader(path);
-  // Check if file doesn't exist;
-  while (!file_reader) {
-    std::cout << "File doesn't exist." << std::endl;
-    std::cout << "Enter file path of file to save trained model:" << std::endl;
-    std::getline(std::cin, path);
-    file_reader.open(path);
+  model.CalculatePriorProbabilities();
+  model.CalculateFeatureProbabilities();
+
+  prior_probabilities_ = model.GetPriorProbabilities();
+  feature_probabilities_ = model.GetFeatureProbabilities();
+
+  WriteToFile(saved_path, prior_probabilities_, feature_probabilities_);
+
+  file_reader.close();
+
+  if (test) {
+    Classify();
+  }
+}
+
+void Driver::LoadModel(const std::string &saved_path) {
+  std::ifstream file_reader(saved_path);
+
+  prior_probabilities_.clear();
+  feature_probabilities_.clear();
+
+  std::string line;
+  std::getline(file_reader, line);
+
+  while (!line.empty()) {
+    std::vector<std::string> tokens = SplitString(line, kSpace);
+    prior_probabilities_.insert(
+        std::make_pair(std::stoi(tokens[0]), std::stod(tokens[1])));
   }
 
-  // Save data if file is empty
-  // https://stackoverflow.com/questions/2390912/checking-for-an-empty-file-in-c
-  if (file_reader.peek() == std::ifstream::traits_type::eof()) {
-    model.CalculateClassProbabilities();
-    model.CalculateFeatureProbabilities();
-
-    WriteToFile(path, model.GetClassProbabilities(),
-                model.GetFeatureProbabilities());
-  } else {
-    std::cout << "File already contains model data." << std::endl;
+  int j = 0;
+  while (!file_reader.eof()) {
+    std::getline(file_reader, line);
+    size_t class_value = std::stoi(line);
+    std::vector<std::vector<double>> features;
+    while (!line.empty()) {
+      std::vector<std::string> tokens;
+      std::vector<double> row;
+      for (size_t i = 0; i < tokens.size(); i++) {
+        row.push_back(std::stod(tokens[i]));
+      }
+      features.push_back(row);
+    }
+    feature_probabilities_.insert(std::make_pair(class_value, features));
+    std::cout << j++ << std::endl;
   }
 
   file_reader.close();
 }
 
-void Driver::WriteToFile(std::string &path,
-                       const std::vector<double> &class_probabilities,
-                       const std::vector<std::vector<std::vector<double>>>
-                           &feature_probabilities) {
-  std::ofstream file_writer(path);
+void Driver::Classify() {
+  Classifier classifier(prior_probabilities_, feature_probabilities_);
+  classifier.ValidateModel();
+}
 
-  // Write the class probabilities first
-  for (double probability : class_probabilities) {
-    file_writer << probability << std::endl;
+void Driver::WriteToFile(
+    const std::string &saved_path,
+    const std::map<size_t, double> &prior_probabilities,
+    const std::map<size_t, std::vector<std::vector<double>>>
+        &feature_probabilities) {
+  std::ofstream file_writer(saved_path);
+
+  // Write the priors first
+  for (const auto &label_image : prior_probabilities) {
+    file_writer << label_image.first << kSpace
+                << prior_probabilities.at(label_image.first) << std::endl;
   }
   file_writer << std::endl;
 
-  // A row of 2D double vectors for each class
-  for (size_t class_value = 0; class_value < feature_probabilities.size();
-       class_value++) {
-    for (size_t row = 0; row < feature_probabilities[class_value].size();
-         row++) {
-      for (size_t col = 0; col < feature_probabilities[class_value].size();
-           col++) {
-        file_writer << feature_probabilities[class_value][row][col] << " ";
+  for (const auto &label_image : feature_probabilities) {
+    file_writer << label_image.first << std::endl;
+    for (size_t row = 0; row < label_image.second.size(); row++) {
+      for (size_t col = 0; col < label_image.second[row].size(); col++) {
+        file_writer << feature_probabilities.at(label_image.first)[row][col]
+                    << kSpace;
       }
       file_writer << std::endl;
     }
     file_writer << std::endl;
   }
+
   file_writer.close();
 }
+
+
 
 }  // namespace naivebayes
