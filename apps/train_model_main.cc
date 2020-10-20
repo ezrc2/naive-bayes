4d59e7798eadd92588df07d35ac2a8b42bea8f06
ezrc2 <ezchen2556@gmail.com>
2020-10-19 23:23:27 -0700

    Added command line arguments parsing

--- apps/train_model_main.cc
+++ apps/train_model_main.cc
@@ -1,7 +1,23 @@
 #include <core/driver.h>
 
-int main() {
+int main(int argc, char* argv[]) {
   naivebayes::Driver driver;
-  driver.TrainModel();
+  std::string action = argv[2];
+
+  if (action == "train") {
+    // Train and save model only
+    if (argc == 7) {
+      driver.TrainModel(argv[3], argv[4], argv[6], false);
+    }
+    // Train, save, and test model
+    else if (argc == 8 && std::string(argv[7]) == "test") {
+      driver.TrainModel(argv[3], argv[4], argv[6], true);
+    }
+  }
+  // Load and test model
+  if (action == "load" && argc == 4) {
+    driver.LoadModel(argv[3]);
+  }
+
   return 0;
 }
